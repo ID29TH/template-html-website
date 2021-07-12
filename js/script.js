92c7125bd1eef150932806c939da498b29d6293a
THAWATCHAI SAING <ex.ecq.development.st1@gmail.com>
2021-07-12 12:41:45 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+ * JAVA SCRIPT FUNCTION ()
+ * Scripts.js
+ * ECq'Studio
+ * http://ecq-studio.com
+ */
+ 
+jQuery(document).ready(function($) {
+	
+/*//////////////////////////////////////
+ * ABOUT
+/*//////////////////////////////////////
+
+	console.log('HELLO I AM ECq\'Studio');
+
+});
